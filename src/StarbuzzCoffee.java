a8dfccc74e90383de71959f62acf31739b1b081b
lutaoact <lutaoact@hotmail.com>
2015-04-10 14:43:45 +0800

    com.lutaoact.decorator

---
+++ src/StarbuzzCoffee.java
@@ -0,0 +1,30 @@
+import com.lutaoact.decorator.Beverage;
+import com.lutaoact.decorator.DarkRoast;
+import com.lutaoact.decorator.Espresso;
+import com.lutaoact.decorator.HouseBlend;
+import com.lutaoact.decorator.Mocha;
+import com.lutaoact.decorator.Soy;
+import com.lutaoact.decorator.Whip;
+
+
+public class StarbuzzCoffee {
+
+	public static void main(String[] args) {
+		Beverage beverage = new Espresso();
+		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		
+		Beverage beverage2 = new DarkRoast();
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Whip(beverage2);
+		System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
+		Beverage beverage3 = new HouseBlend();
+		beverage3 = new Soy(beverage3);
+		beverage3 = new Mocha(beverage3);
+		beverage3 = new Whip(beverage3);
+		System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
+		
+	}
+
+}
---
+++ src/com/lutaoact/decorator/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package com.lutaoact.decorator;
+
+public abstract class CondimentDecorator extends Beverage {
+	public abstract String getDescription();
+}
